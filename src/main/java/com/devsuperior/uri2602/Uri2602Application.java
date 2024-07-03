12cf0efeebb5f5d854e86e6132908791f493062c
leolm83 <leonardo.mateus01@hotmail.com>
2024-07-03 16:55:44 -0300

    adicionado mais consultas

--- src/main/java/com/devsuperior/uri2602/Uri2602Application.java
+++ src/main/java/com/devsuperior/uri2602/Uri2602Application.java
@@ -5,7 +5,6 @@ import com.devsuperior.uri2602.entities.Customer;
 import com.devsuperior.uri2602.projections.CustomerNameMinProjection;
 import com.devsuperior.uri2602.repositories.CustomerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
@@ -25,11 +24,28 @@ public class Uri2602Application {
 
 	@EventListener(ApplicationReadyEvent.class)
 	public void run() {
-		List<CustomerNameMinProjection> result = customerRepository.searchByState("RS");
+		final String LINE_SEPARATOR = "--------------------------";
+		String state = "RS";
+		System.out.println(LINE_SEPARATOR);
+		System.out.println("COM SQL NORMAL");
+		List<CustomerNameMinProjection> result = customerRepository.searchByState(state);
 		List<CustomerMinDTO> dtos = result.stream().map(item -> new CustomerMinDTO(item)).collect(Collectors.toList());
 		for (CustomerMinDTO customerMinDTO: dtos){
 			System.out.println(customerMinDTO.getName());
 		}
-		System.out.println("acabou!");
+		System.out.println(LINE_SEPARATOR);
+		System.out.println("JPQL SEM USO DE DTO(RETORNA TODOS OS DADOS)");
+		List<Customer> customers = customerRepository.searchByStateWithJPQL(state);
+		for (Customer customer: customers){
+			System.out.println(customer.getName());
+		}
+		System.out.println("--------------------------");
+		System.out.println("JPQL com DTO");
+		List<CustomerMinDTO> dtosJPQL = customerRepository.searchNameByStateWithJPQL(state);
+		for (CustomerMinDTO customerMinDTO: dtosJPQL){
+			System.out.println(customerMinDTO.getName());
+		}
+		System.out.println(LINE_SEPARATOR);
+
 	}
 }
--- src/main/java/com/devsuperior/uri2602/repositories/CustomerRepository.java
+++ src/main/java/com/devsuperior/uri2602/repositories/CustomerRepository.java
@@ -1,5 +1,6 @@
 package com.devsuperior.uri2602.repositories;
 
+import com.devsuperior.uri2602.dtos.CustomerMinDTO;
 import com.devsuperior.uri2602.entities.Customer;
 import com.devsuperior.uri2602.projections.CustomerNameMinProjection;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -12,4 +13,9 @@ import java.util.List;
 public interface CustomerRepository extends JpaRepository<Customer,Long> {
     @Query(value = "SELECT * FROM CUSTOMERS WHERE STATE = :state",nativeQuery = true)
     List<CustomerNameMinProjection> searchByState(String state);
+    @Query(value = "SELECT obj FROM Customer obj WHERE obj.state = :state")
+    List<Customer> searchByStateWithJPQL(String state);//busca todos os dados
+    @Query(value = "SELECT new com.devsuperior.uri2602.dtos.CustomerMinDTO(obj.name) FROM Customer obj WHERE obj.state = :state")
+    List<CustomerMinDTO> searchNameByStateWithJPQL(String state);//busca todos os dados
+
 }
